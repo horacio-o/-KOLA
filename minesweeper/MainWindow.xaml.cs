ff744d6475893fd7583da03e235872dccfc65bba
horacio-o <horant05@gmail.com>
2025-02-17 19:13:14 +0100

    posun v minesweeper

---
+++ minesweeper/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Timers;
+
+namespace minesweeper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void EasyClick(object sender, RoutedEventArgs e)
+        {
+            Expander.Visibility = Visibility.Collapsed;
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+            vm.GenerateButtons(grid, 8, 10);
+        }
+
+        private void MediumClick(object sender, RoutedEventArgs e)
+        {
+            Expander.Visibility = Visibility.Collapsed;
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+            vm.GenerateButtons(grid, 14, 40);
+        }
+
+        private void HardcoreClick(object sender, RoutedEventArgs e)
+        {
+            Expander.Visibility = Visibility.Collapsed;
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
+            vm.GenerateButtons(grid, 20,99);
+        }
+    }
+}
---
+++ minesweeper/MainWindowViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace minesweeper
+{
+    class Button2 : Button
+    {
+        public bool isMine = false; 
+        public int Value = 0;
+    }
+    internal class MainWindowViewModel
+    {
+
+
+        public Button2[,] GenerateButtons(Grid grid, int size, int numberOfMines)
+        {
+            Button2[,] buttons = new Button2[size, size];
+            for (int i = 0; i < size; i++)//vytvoří buttons
+            {
+                grid.ColumnDefinitions.Add(new ColumnDefinition());
+                grid.RowDefinitions.Add(new RowDefinition());
+                for (int j = 0; j < size; j++)
+                {
+                    Button2 button = new Button2();
+                    button.Click += ButtonClicked;
+
+                    // Nastavení pozice v Gridu
+                    Grid.SetRow(button, i + 1);
+                    Grid.SetColumn(button, j + 1);
+
+                    // Přidání tlačítka do Gridu
+                    grid.Children.Add(button);
+
+                    // Přidání tlačítka do pole tlačítek
+                    buttons[i, j] = button;
+                }
+            }
+            Random rnd = new Random();
+            for (int i = 0; i < numberOfMines; i++) //určí náhodně, které buttons jsou miny. Je teoreticky možné, že generace min bude trvat nekonečně dlouhou dobu, ale to se nestane 
+            {
+                int x_value = rnd.Next(size);
+                int y_value = rnd.Next(size);
+                if(!buttons[x_value, y_value].isMine)
+                {
+                    buttons[x_value, y_value].isMine = true;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+            return buttons;
+        }
+        //TODO: 1. right click 2. získání hodnoty, když na to kliknu 3. když je hodnota 0, udělat BFS
+
+        private void ButtonClicked(object sender, RoutedEventArgs e)
+        {
+            if(((Button2)sender).isMine)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                ((Button2)sender).Content = ((Button2)sender).Value;
+            }
+        } 
+    }
+}
