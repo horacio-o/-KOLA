bb1d521c302e6bd016d6da9ddc6d85234736ea73
horacio-o <horant05@gmail.com>
2025-03-08 23:03:33 +0100

    coins

--- Coins/Program.cs
+++ Coins/Program.cs
@@ -6,13 +6,42 @@ using System.Threading.Tasks;
 
 namespace Coins
 {
-    internal class Program
+    static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int[] coins = Int32.Parse(Console.ReadLine().Split(' '));
-            
+            int[] coins = Console.ReadLine().Split(' ').Select(Int32.Parse).ToArray();
+            int target = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            if (target <= 0)
+            {
+                Console.WriteLine("Nejde to :(");
+                return;
+            }
+            bool that = false;
+            Coining(coins, target, new List<int>(), 0, 0, ref that);
+
+            if (!that)
+            {
+                Console.WriteLine("Nejde to :(");
+            }
         }
-        
+        static void Coining(int[] coins, int target, List<int> so_far, int sum, int index, ref bool found_something)
+        {
+            if (sum > target) return;
+            if (sum == target)
+            {
+                Console.WriteLine(string.Join(" ", so_far));
+                found_something = true;
+                return;
+            }
+            for (int i = index; i < coins.Length; i++)
+            {
+                so_far.Add(coins[i]);
+                Coining(coins, target, so_far, sum + coins[i], i, ref found_something);
+                so_far.RemoveAt(so_far.Count - 1);
+            }
+        }
     }
 }
