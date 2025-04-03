7a43470c7a15eceed961bd8ca12fd4b354cca9a0
horacio-o <horant05@gmail.com>
2025-04-04 00:58:06 +0200

    :(

--- 0-1 batoh/Program.cs
+++ 0-1 batoh/Program.cs
@@ -12,14 +12,37 @@ namespace _0_1_batoh
         {
             int[] weights = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] values = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            int max_value = int.Parse(Console.ReadLine());
+            int max_weight = int.Parse(Console.ReadLine());
+
+            bool found_something = false;
+            
+            Batoh(weights, values, max_weight, 0, 0, new List<int>(), 0, ref found_something, new List<int>(), 0);
+
+            Console.Read();
         }
 
-        static void Batoh(int[] weights, int[] values, int max_value, int current_sum, int index, List<int> values_so_far, ref bool something_found)
+        static void Batoh(int[] weights, int[] values, int max_weight, int current_weight, int index, List<int> values_so_far, int current_sum, ref bool something_found, List<int> best_sequence_so_far, int best_sum_so_far)
         {
-            if(current_sum > max_value)
+            if(current_weight > max_weight)
             {
                 return;
+            }
+            if(current_sum > best_sum_so_far)
+            {
+                something_found = true;
+                best_sequence_so_far = values_so_far;
+                best_sum_so_far = current_sum;
+                //return;
+            }
+            for (int i = index; i < weights.Length; i++)
+            {
+                values_so_far.Add(i + 1);
+                current_sum = current_sum + values[i];
+                current_weight = current_weight + weights[i];
+                Batoh(weights, values, max_weight, current_weight, i /* to je index*/, values_so_far, current_sum, ref something_found, best_sequence_so_far, best_sum_so_far);
+                values_so_far.RemoveAt(values_so_far.Count - 1);
+                current_sum = current_sum - values[i];
+                current_weight = current_weight - weights[i];
             }
         }
     }
